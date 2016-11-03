cabe4c3e8e2b92e6125a73047d0420951e78c247
bradleyal53 <bradleyal@cwu.edu>
2016-11-03 09:54:50 -0700

    Added functionality
    Does not throw exceptions

--- calculator.java
+++ calculator.java
@@ -3,8 +3,10 @@ import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -227,179 +229,269 @@ public class calculator {
 		}); 
 	} // end initialize 
 	
+	/** 
+	 * Adds functionality to zero button
+	*/ 
 	private void zeroButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + zeroButton.getText());
 		initialResult.append(zeroButton.getText()); 
 	} // end zeroButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to one button
+	*/ 
 	private void oneButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[0].getText());
 		initialResult.append(numberButtons[0].getText()); 
 	} // end oneButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to two button
+	*/ 
 	private void twoButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[1].getText());
 		initialResult.append(numberButtons[1].getText()); 
 	} // end two ButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to three button
+	*/ 
 	private void threeButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[2].getText());
 		initialResult.append(numberButtons[2].getText()); 
 	} // end threeButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to four button
+	*/ 
 	private void fourButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[3].getText());
 		initialResult.append(numberButtons[3].getText()); 
 	} // end fourButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to five button
+	*/ 
 	private void fiveButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[4].getText());
 		initialResult.append(numberButtons[4].getText()); 
 	} // end fiveButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to six button
+	*/ 
 	private void sixButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[5].getText());
 		initialResult.append(numberButtons[5].getText()); 
 	} // end sixButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to seven button
+	*/ 
 	private void sevenButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[6].getText());
 		initialResult.append(numberButtons[6].getText()); 
 	} // end sevenButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to eight button
+	*/ 
 	private void eightButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[7].getText());
 		initialResult.append(numberButtons[7].getText()); 
 	} // end eightButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to nine button
+	*/ 
 	private void nineButtonActionPerformed(ActionEvent event){
 		text.setText(text.getText() + numberButtons[8].getText());
 		initialResult.append(numberButtons[8].getText()); 
 	} // end nineButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to addition button
+	*/ 
 	private void addOppActionPerformed(ActionEvent event){
 		text.setText(text.getText() + addOpp.getText()); 
 		initialResult.append(addOpp.getText()); 
 	} // end addOppButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to subtraction button
+	*/ 
 	private void subOppActionPerformed(ActionEvent event){
 		text.setText(text.getText() + subOpp.getText()); 
 		initialResult.append(subOpp.getText()); 
 	} // end subOppButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to multiplication button
+	*/ 
 	private void multOppActionPerformed(ActionEvent event){
 		text.setText(text.getText() + multOpp.getText()); 
 		initialResult.append(multOpp.getText()); 
 	} // end multOppButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to division button
+	*/ 
 	private void divOppActionPerformed(ActionEvent event){
 		text.setText(text.getText() + divOpp.getText()); 
 		initialResult.append(divOpp.getText()); 
 	} // end divOppButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to power button
+	*/ 
 	private void powOppActionPerformed(ActionEvent event){
 		text.setText(text.getText() + powOpp.getText());
 		initialResult.append(powOpp.getText()); 
 	} // end powOppButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to leftPOpp button
+	*/ 
 	private void leftPOppActionPerformed(ActionEvent event){
 		text.setText(text.getText() + leftPOpp.getText());
 		initialResult.append(leftPOpp.getText()); 
 	} // end leftPOppButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to rightPOpp button
+	*/ 
 	private void rightPOppActionPerformed(ActionEvent event){
 		text.setText(text.getText() + rightPOpp.getText()); 
 		initialResult.append(rightPOpp.getText()); 
 	} // end rightsPOppButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to clear button
+	*/ 
 	private void clearButtonActionPerformed(ActionEvent event){
 		text.setText(""); 
 		initialResult.setLength(0);
 	} // end clearButtonActionPerformed
 	
+	/** 
+	 * Adds functionality to equals button
+	*/ 
 	private void equalsButtonActionPerformed(ActionEvent event){
-		text.setText("result: " + evalPost(infixToPostfix(initialResult.toString())));
+		text.setText("result: " + evalPost(convertToPostfix(initialResult.toString())));
 	} // end equalsButtonActionPerformed
-
-	static String infixToPostfix(String infix) {
-        final String ops = "-+/*^";
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> s = new Stack<>();
- 
-        for (String token : infix.split("")) {
-            if (token.isEmpty())
-                continue;
-            char c = token.charAt(0);
-            int idx = ops.indexOf(c);
- 
-            // check for operator
-            if (idx != -1) {
-                if (s.isEmpty())
-                    s.push(idx);
- 
-                else {
-                    while (!s.isEmpty()) {
-                        int prec2 = s.peek() / 2;
-                        int prec1 = idx / 2;
-                        if (prec2 > prec1 || (prec2 == prec1 && c != '^'))
-                            sb.append(ops.charAt(s.pop())).append(' ');
-                        else break;
-                    }
-                    s.push(idx);
-                }
-            } 
-            else if (c == '(') {
-                s.push(-2); // -2 stands for '('
-            } 
-            else if (c == ')') {
-                // until '(' on stack, pop operators.
-                while (s.peek() != -2)
-                    sb.append(ops.charAt(s.pop())).append(' ');
-                s.pop();
-            }
-            else {
-                sb.append(token).append(' ');
-            }
-        }
-        while (!s.isEmpty())
-            sb.append(ops.charAt(s.pop())).append(' ');
-        return sb.toString();
-    } // end infixToPostfix 
 	
-	private String evalPost(String string){
-		Stack<Double> secStack = new Stack(); 
-		Scanner scan = new Scanner(string); 
-		while (scan.hasNext()){
-			if(scan.hasNextInt()){
-				secStack.push(scan.nextDouble()); 
-			} // end if 
-			else {
-				double num2 = secStack.pop(); 
-				double num1 = secStack.pop();
-				String ops = scan.next(); 
-				
-				switch(ops){
-				case "+": 
-					secStack.push(num1 + num2); 
-					break; 
-				case "-": 
-					secStack.push(num1 - num2); 
-					break; 
-				case "*": 
-					secStack.push(num1 * num2); 
-					break; 
-				case "/": 
-					secStack.push(num1 / num2); 
-					break; 
-				case "^": 
-					secStack.push( Math.pow(num1, num2)); 
-					break; 
-				default: 
-					return null;  
-				} // end switch
-				return Double.toString(secStack.pop());
-			} // else 
-		} // end while		
-		return null; 
-	} // end evalPost
+	/** 
+	 * Determines if a character is an operator and returns that
+	*/ 
+	private boolean isOperator(char a) { // Tell whether c is an operator.
+
+	     return a == '+'  ||  a == '-'  ||  a == '*'  ||  a == '/'  ||  a == '^'
+	           || a =='(' || a ==')';
+	   
+	   }//end isOperator
+
+	/** 
+	 * Tells whether cha1 has a lower precedence than cha2 
+	 * Returns a boolean value
+	*/ 
+	   private boolean lowerPrecedence(char cha1, char cha2) {
+	      switch (cha1) {
+
+	         case '+':
+	         case '-':
+	            return !(cha2=='+' || cha1=='-') ;
+
+	         case '*':
+	         case '/':
+	            return cha2=='^' || cha1=='(';
+
+	         case '^':
+	            return cha2=='(';
+
+	         case '(': return true;
+
+	         default:  // won't happen 
+	            return false;
+	      } // end switch 
+	   } // end lowerPrecedence
+
+	   /** 
+		 * Converts a string to postfix notation 
+		 * Returns a string
+		*/ 
+	   public String convertToPostfix(String infix) {
+		   Stack operatorStack = new Stack<>();  
+		   char c;  
+		   StringTokenizer parser = new StringTokenizer(infix,"+-*/^() ",true);
+	       StringBuffer postfix = new StringBuffer(infix.length()); // holds the result 
+	       while (parser.hasMoreTokens()) {     
+	    	   String token = parser.nextToken();          
+	           c = token.charAt(0); 
+	           if ( (token.length() == 1) && isOperator(c) ) 
+	           {
+	              while (!operatorStack.empty() &&
+	                  !lowerPrecedence(((String)operatorStack.peek()).charAt(0), c)){
+	            	  postfix.append(" ").append((String)operatorStack.pop());
+	              } // end while
+	              if (c==')') {
+	                 String operator = (String)operatorStack.pop();
+	                 while (operator.charAt(0)!='(') {
+	                	 postfix.append(" ").append(operator);
+	                     operator = (String)operatorStack.pop();  
+	                    } // end while
+	              } // end if 
+	              else
+	                 operatorStack.push(token);
+	           } // end if 
+	           else {
+	             postfix.append(" ").append(token);
+	           }//end if
+	         }// end while
+	 
+	     // Output the remaining operators on the stack.
+	        while (!operatorStack.empty())
+	           postfix.append(" ").append((String)operatorStack.pop());
+	     // Return the result.
+	        return postfix.toString();
+	   }//end convertToPostfix
+	   
+	   /** 
+		 * Evaluates the postfix notation 
+		 * Returns the final result string
+		*/ 
+		private String evalPost(String string){
+			Stack<Double> secStack = new Stack(); 
+			Scanner scan = new Scanner(string); 
+			while (scan.hasNext()){
+				if(scan.hasNextInt()){
+					secStack.push(scan.nextDouble()); 
+				} // end if 
+				else {
+					double num2 = secStack.pop(); 
+					double num1 = secStack.pop();
+					String ops = scan.next(); 
+					
+					switch(ops){
+					case "+": 
+						secStack.push(num1 + num2); 
+						break; 
+					case "-": 
+						secStack.push(num1 - num2); 
+						break; 
+					case "*": 
+						secStack.push(num1 * num2); 
+						break; 
+					case "/": 
+						secStack.push(num1 / num2); 
+						break; 
+					case "^": 
+						secStack.push(Math.pow(num1, num2)); 
+						break; 
+					default: 
+						return null;  
+					} // end switch
+				} // else 
+			} // end while
+			return Double.toString(secStack.pop());
+		} // end evalPost */ 
 } // end calculator 
